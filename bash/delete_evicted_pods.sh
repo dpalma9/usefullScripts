78620b9c5aea90465917fb30080b4e64a382979e
dpalma9 <dpalma@IM1825.morse.local>
2020-10-23 11:12:18 +0100

    Fix namespace

--- bash/delete_evicted_pods.sh
+++ bash/delete_evicted_pods.sh
@@ -1,7 +1,7 @@
 #/bin/bash!
 
-kubectl get pods -n cxb-obsicp-pro |awk '{ print $1}' > evicted_pods.txt
+kubectl get pods -n my-namespace |awk '{ print $1}' > evicted_pods.txt
 cat evicted_pods | while read var1;do
    #echo "$var1"
-   kubectl delete pod -n cxb-obsicp-pro $var1
+   kubectl delete pod -n my-namespace $var1
 done
